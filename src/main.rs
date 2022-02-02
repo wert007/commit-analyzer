3aecca2033f2c5faa3d27b45805a8c96465a789e
wert007 <pablohamacher@web.de>
2022-02-02 13:13:47 +0100

    Implement basic filter

--- src/main.rs
+++ src/main.rs
@@ -32,11 +32,11 @@ fn main() {
     }
     let mut last_time = None;
     let mut duration = chrono::Duration::zero();
-    let filter = Filter::none();
+    let filter = Filter::default();
     for commit in parsed_commits.into_iter().rev() {
         if matches_filter(&commit, &filter) {
             if let Some(last_time) = last_time {
-                let diff : chrono::Duration = commit.date - last_time;
+                let diff: chrono::Duration = commit.date - last_time;
                 if diff.num_hours() <= 3 {
                     duration = duration + diff;
                 }
@@ -57,17 +57,59 @@ fn usage(program_name: &str, opts: &getopts::Options) {
 }
 
 fn matches_filter(commit: &Commit, filter: &Filter) -> bool {
-    true
+    filter.check_author_name(&commit.author.name)
+        && filter.check_author_email(&commit.author.email)
+        && filter.check_commit(&commit.commit)
+        && filter.check_message(&commit.message)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Filter {
-
+    author_equals: Vec<String>,
+    author_contains: Vec<String>,
+    email_equals: Vec<String>,
+    email_contains: Vec<String>,
+    commit_equals: Vec<String>,
+    commit_contains: Vec<String>,
+    message_equals: Vec<String>,
+    message_contains: Vec<String>,
+    message_starts_with: Vec<String>,
 }
 
 impl Filter {
-    pub fn none() -> Self {
-        Self {}
+    fn check_author_name(&self, name: &str) -> bool {
+        let equals = self.author_equals.is_empty() || self.author_equals.iter().any(|n| n == name);
+        let contains = self.author_contains.is_empty()
+            || self.author_contains.iter().any(|n| name.contains(n));
+        equals && contains
+    }
+
+    fn check_author_email(&self, email: &str) -> bool {
+        let equals = self.email_equals.is_empty() || self.email_equals.iter().any(|e| e == email);
+        let contains =
+            self.email_contains.is_empty() || self.email_contains.iter().any(|e| email.contains(e));
+        equals && contains
+    }
+
+    fn check_commit(&self, commit: &str) -> bool {
+        let equals =
+            self.commit_equals.is_empty() || self.commit_equals.iter().any(|c| c == commit);
+        let contains = self.commit_contains.is_empty()
+            || self.commit_contains.iter().any(|c| commit.contains(c));
+        equals && contains
+    }
+
+    fn check_message(&self, message: &str) -> bool {
+        let equals =
+            self.message_equals.is_empty() || self.message_equals.iter().any(|m| m == message);
+        let contains = self.message_contains.is_empty()
+            || self.message_contains.iter().any(|m| message.contains(m));
+        let starts_with = self.message_starts_with.is_empty()
+            || self
+                .message_starts_with
+                .iter()
+                .any(|m| message.starts_with(m));
+        equals && contains && starts_with
     }
 }
 
