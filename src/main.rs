90aaaa2257f48b364567a8f9b825d36dd2367b95
wert007 <pablohamacher@web.de>
2022-02-05 11:29:01 +0100

    Add commits per day analysis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-fn main() {
+use std::{collections::HashMap, ops::AddAssign, error::Error, io::Write};
+
+fn main() -> Result<(), Box<dyn Error>>{
     let mut opts = getopts::Options::new();
     let opts = opts
         .optflag("q", "quiet", "Hides the output of commit messages")
@@ -61,27 +63,34 @@ fn main() {
             "duration",
             "The time, which may pass between two commits, that still counts as working.",
             "HOURS",
+        )
+        .optopt(
+            "o",
+            "output",
+            "An output file for the commits per day in csv format.",
+            "FILE",
         );
     let matches = match opts.parse(std::env::args()) {
         Ok(it) => it,
-        Err(_) => {
+        Err(err) => {
             usage("commit-analzer", opts);
-            return;
+            return Err(err.into());
         }
     };
     if matches.free.len() < 2 {
         usage(&matches.free[0], opts);
-        return;
+        return Ok(());
     }
     let is_quiet = matches.opt_present("q");
     let max_diff_hours : u32 = match matches.opt_str("duration").map(|str| str.parse()) {
         None => 3,
         Some(Ok(it)) => it,
-        Some(Err(_)) => {
+        Some(Err(err)) => {
             eprintln!("duration must be an integer value!");
-            return;
+            return Err(err.into());
         },
     };
+    let path = matches.opt_str("output");
     let commits_path = &matches.free[1];
     let commits = std::fs::read_to_string(commits_path).unwrap();
     let mut commits = commits.as_str();
@@ -113,9 +122,11 @@ fn main() {
         message_starts_with: matches.opt_strs("message-starts-with"),
     };
     let mut commit_count = 0;
+    let mut commits_per_day = HashMap::new();
     for commit in parsed_commits.into_iter().rev() {
         if matches_filter(&commit, &filter) {
             commit_count += 1;
+            commits_per_day.entry(commit.date.date()).or_insert(0).add_assign(1);
             if let Some(last_time) = last_time {
                 let diff: chrono::Duration = commit.date - last_time;
                 if diff.num_hours() <= max_diff_hours as i64 {
@@ -131,6 +142,15 @@ fn main() {
 
     println!("Estimated time was {}h", duration.num_hours());
     println!("Found {} commits overall", commit_count);
+
+    if let Some(path) = path {
+        let mut file = std::fs::File::create(path)?;
+        for (key, value) in commits_per_day {
+            writeln!(file, "{}, {}", key, value)?;
+        }
+    }
+
+    Ok(())
 }
 
 fn usage(program_name: &str, opts: &getopts::Options) {
