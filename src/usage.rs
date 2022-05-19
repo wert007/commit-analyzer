f580e792cb180de12c729d538db21afd52343918
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-05-19 18:10:39 +0200

    Fix typo:  wrong parameter name

--- src/usage.rs
+++ src/usage.rs
@@ -1,4 +1,4 @@
-pub fn usage (app_name: & str, opts: & getopts :: Options)
+pub fn usage (app_name: & str, options: & getopts :: Options)
 {
     println ! ("Usage: {} <FILE> [OPTIONS]", app_name);
     println ! ("{}", options.usage ("Parses the output of `git log`."));
