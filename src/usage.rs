c9ed9e791a01272709c7c6a16af0c87915b38b08
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-05-19 19:26:08 +0200

    Hard code application name

--- src/usage.rs
+++ src/usage.rs
@@ -2,7 +2,8 @@ use getopts :: Options;
 
 pub fn usage (app_name: & str, options: & Options) -> ()
 {
-    println ! ("Usage: {} <FILE> [OPTIONS]", app_name);
-    println ! ("{}", options.usage ("Parses the output of `git log`."));
+    println ! ( "Usage: commit-analyzer <FILE> [OPTIONS]\n{}"
+              , options.usage ("Parses the output of `git log`.")
+              );
     return;
 }
