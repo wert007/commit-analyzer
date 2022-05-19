383b3c2b1c92f62c8898d5ae08f163dd1cc54b5e
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-05-19 19:54:52 +0200

    Document function

--- src/usage.rs
+++ src/usage.rs
@@ -1,5 +1,11 @@
 use getopts :: Options;
 
+/// A small in-app documentation.
+///
+/// * `options` - The options the render the tool tips for.
+///
+/// This function will write a brief usage information, including a short
+/// introduction to the meaning of the configured options, to `stdout`.
 pub fn usage (options: & Options) -> ()
 {
     println ! ( "Usage:  commit-analyzer <FILE> [OPTIONS]\n{}"
