962bf786b139a851adcec8f6139563084cb5f7b2
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-05-19 19:24:32 +0200

    Load required module appropriately

--- src/usage.rs
+++ src/usage.rs
@@ -1,4 +1,6 @@
-pub fn usage (app_name: & str, options: & getopts :: Options) -> ()
+use getopts :: Options;
+
+pub fn usage (app_name: & str, options: & Options) -> ()
 {
     println ! ("Usage: {} <FILE> [OPTIONS]", app_name);
     println ! ("{}", options.usage ("Parses the output of `git log`."));
