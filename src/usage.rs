8db2f263a2562245e19ff638870da8275b93a0b1
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-05-19 18:28:55 +0200

    Make the functon `return` at the end explicitly

--- src/usage.rs
+++ src/usage.rs
@@ -1,5 +1,6 @@
-pub fn usage (app_name: & str, options: & getopts :: Options)
+pub fn usage (app_name: & str, options: & getopts :: Options) -> ()
 {
     println ! ("Usage: {} <FILE> [OPTIONS]", app_name);
     println ! ("{}", options.usage ("Parses the output of `git log`."));
+    return;
 }
