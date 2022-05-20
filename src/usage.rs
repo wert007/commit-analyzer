bf4adf5333858501a5c5bd195249db810fe596f6
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-05-20 15:52:12 +0200

    Adjust function documentation

--- src/usage.rs
+++ src/usage.rs
@@ -11,10 +11,8 @@ use getopts::Options;
 
 /// A small in-app documentation.
 ///
-/// * `options` - The options the render the tool tips for.
-///
 /// This function will write a brief usage information, including a short
-/// introduction to the meaning of the configured options, to `stdout`.
+/// introduction to the meaning of the configured `options`, to `stdout`.
 pub fn usage(options: &Options) {
     println!(
         "Usage:  commit-analyzer <FILE> [OPTIONS]\n\n{}",
