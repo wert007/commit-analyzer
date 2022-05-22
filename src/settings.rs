b40dd112529a32b98f5cc6b3e57795b3137d1e36
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-05-22 23:40:47 +0200

    Add constants

--- src/settings.rs
+++ src/settings.rs
@@ -4,10 +4,30 @@
 //! in order to avoid redundancy.
 
 /// This application's name.
+#[allow(dead_code)]
 pub const APP_NAME: &str = "commit-analyzer";
 
 /// This application's tool tip.
 ///
 /// In order to show a brief usage synopsis to the user, this global variable
 /// sets the information about the required and optional command line arguments.
+#[allow(dead_code)]
 pub const APP_TOOL_TIP: &str = "<FILE> [OPTIONS]";
+
+/// This application's version information.
+///
+/// This constant represents this application's major version.
+#[allow(dead_code)]
+pub const VERSION_MAJOR: u8 = 0u8;
+
+/// This application's version information.
+///
+/// This constant represents this application's minor version.
+#[allow(dead_code)]
+pub const VERSION_MINOR: u8 = 1u8;
+
+/// This application's version information.
+///
+/// This constant represents this application's fix level.
+#[allow(dead_code)]
+pub const VERSIO_FIX_LEVEL: u8 = 0u8;
