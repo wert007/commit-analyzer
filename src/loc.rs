54cb16942b60b82e808d53c50174f501402dcde7
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-05-23 17:56:26 +0200

    Move loc related data structures

--- src/loc.rs
+++ src/loc.rs
@@ -1,1 +1,57 @@
 //! The `Loc` struct and related utilities.
+
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+pub enum LocParseError {
+    FirstTabulatorMissing,
+    SecondTabulatorMissing,
+    AddedParseError(ParseIntError),
+    RemovedParseError(ParseIntError),
+}
+
+#[derive(Debug)]
+pub struct Loc {
+    added: Option<u32>,
+    removed: Option<u32>,
+    file: String,
+}
+
+impl Loc {
+    pub fn file(&self) -> String {
+        String::from(&self.file)
+    }
+
+    pub fn parse(loc: &str) -> Result<Self, LocParseError> {
+        let (added, remainder) = loc
+            .split_once('\t')
+            .ok_or(LocParseError::FirstTabulatorMissing)?;
+        let (removed, file) = remainder
+            .split_once('\t')
+            .ok_or(LocParseError::SecondTabulatorMissing)?;
+        let added = if added == "-" {
+            None
+        } else {
+            Some(added.parse().map_err(LocParseError::AddedParseError)?)
+        };
+        let removed = if removed == "-" {
+            None
+        } else {
+            Some(removed.parse().map_err(LocParseError::RemovedParseError)?)
+        };
+        let file = file.into();
+        Ok(Self {
+            added,
+            removed,
+            file,
+        })
+    }
+
+    pub fn loc(&self) -> i64 {
+        if self.added.is_none() && self.removed.is_none() {
+            0
+        } else {
+            self.added.unwrap() as i64 - self.removed.unwrap() as i64
+        }
+    }
+}
