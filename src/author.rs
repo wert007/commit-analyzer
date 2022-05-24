4eb36d092699ce4acd253aa95dc7e17701fc1e57
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-05-24 16:47:20 +0200

    Move author related symbols to dedicated module

--- src/author.rs
+++ src/author.rs
@@ -1,1 +1,36 @@
+#[derive(Debug)]
+pub struct Author {
+    name: String,
+    email: String,
+}
 
+impl Author {
+    pub fn email(&self) -> String {
+        String::from(&self.email)
+    }
+
+    pub fn name(&self) -> String {
+        String::from(&self.name)
+    }
+
+    pub fn parse(author: &str) -> Result<Author, AuthorParseError> {
+        let (name, remainder) = author.split_once('<').ok_or(AuthorParseError::NameFailed)?;
+
+        let email = remainder
+            .strip_suffix('>')
+            .ok_or(AuthorParseError::EmailFailed)?
+            .trim()
+            .into();
+
+        Ok(Self {
+            name: name.trim().into(),
+            email,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum AuthorParseError {
+    NameFailed,
+    EmailFailed,
+}
